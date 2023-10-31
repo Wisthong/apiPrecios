5d4b9c3d747cdb26227c3d7051a27eaa006cd167
Wisthong Martinez <wisthongd@gmail.com>
2023-10-31 16:42:46 -0500

    Inner join sql puro

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 require("dotenv").config();
+const mysql = require("mysql2");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { dbConnectMySQL } = require("./db/sql/connection");
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
+});
+
+connection.query(
+  "SELECT ITEMS.ID_ITEM, ITEMS.ID_CODBAR, CMLISTA_PRECIOS_D.ID_LIPRE1,CMLISTA_PRECIOS_D.PRECIO_MIN_1 FROM ITEMS INNER JOIN CMLISTA_PRECIOS_D ON ITEMS.ID_ITEM=CMLISTA_PRECIOS_D.ID_ITEM",
+  function (err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+
 /**
  * API Rest
  */
-app.use("/api", require("./routes"));
+// app.use("/api", require("./routes"));
 
 app.listen(port, () =>
   console.log(`Tu server esta listo por el puerto ${port}`)
@@ -21,4 +36,4 @@ app.listen(port, () =>
  * Define your database engine
  */
 
-dbConnectMySQL();
+// dbConnectMySQL();
