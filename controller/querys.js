bdaa6a9796cf65adbf214f91ba9a748c13341aaa
Wisthong D Martinez Castro <wisthongd@gmail.com>
2025-03-03 17:01:17 -0500

    Ajustando CO

--- controller/querys.js
+++ controller/querys.js
@@ -15,34 +15,10 @@ const postDO = async (req = request, res = response) => {
       });
     }
 
-    // const query = `
-    //   SELECT MV.LAPSO_DOC AS Lapso,
-    //     MV.ID_CO AS "Centro de Operacion",
-    //     CO.DESCRIPCION AS "Nombre Centro de Operacion",
-    //     MV.ID_ITEM AS Item,
-    //     ITMS.DESCRIPCION AS "Nombre Item",
-    //     ITMS.ID_TERC AS "Proveedor Codigo Item",
-    //     ITMS.NOM_TERC AS "Proveedor Nombre Item",
-    //     MV.FECHA_DCTO AS Fecha,
-    //     MV.ID_LIDES AS "Lista Descuento",
-    //     MV.CANTIDAD_CAP AS "Cantidad 1",
-    //     MV.DSCTO_NETOS AS "Valor Descuentos",
-    //     MV.TOT_BRUTO AS "Valor Neto"
-    //   FROM
-    //     ITEMS ITMS
-    //     RIGHT OUTER JOIN (CENTRO_OPERACION CO
-    //     RIGHT OUTER JOIN MOVIMIENTO_VENTAS MV ON CO.CODIGO = MV.ID_CO)
-    //     ON ITMS.ID_ITEM = MV.ID_ITEM AND ITMS.ID_EXT_ITM = MV.ID_EXT_ITM
-    //   WHERE
-    //     MV.ID_ITEM = ?
-    //     AND MV.FECHA_DCTO BETWEEN ? AND ?
-    //     AND MV.ID_LIDES IS NOT NULL
-    //     AND MV.ID_LIDES != ''
-    // `;
-
     const query = `
     SELECT 
       MV.ID_ITEM AS item,
+      MV.ID_CO AS centro_operacion,
       ITMS.DESCRIPCION AS "nombre_item",
       ITMS.ID_TERC AS "proveedor_codigo",
       ITMS.NOM_TERC AS "proveedor_nombre",
@@ -56,6 +32,7 @@ const postDO = async (req = request, res = response) => {
       ON ITMS.ID_ITEM = MV.ID_ITEM AND ITMS.ID_EXT_ITM = MV.ID_EXT_ITM
     WHERE 
       MV.ID_ITEM = ? 
+      AND MV.ID_CO = ?
       AND MV.FECHA_DCTO BETWEEN ? AND ?
       AND MV.ID_LIDES IS NOT NULL 
       AND MV.ID_LIDES != ''
@@ -66,13 +43,17 @@ const postDO = async (req = request, res = response) => {
       [id_item, lapso_inicio, lapso_fin],
       function (err, results, fields) {
         if (!err) {
-          console.log(fields);
-          console.log(results);
-
-          res.send({
-            ok: true,
-            data: results,
-          });
+          if (results[0].item !== null) {
+            res.send({
+              ok: true,
+              data: results,
+            });
+          } else {
+            res.send({
+              ok: false,
+              data: results,
+            });
+          }
         } else {
           res.status(500).send({
             message: "Query_ERROR_204",
